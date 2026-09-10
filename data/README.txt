43dbdadafa85c9816229149f38c1c57c4cfcc33f
Dennis Turner <mugambwadennistt@gmail.com>
2026-09-10 15:51:30 +0300

    Remove all files from repository

--- data/README.txt
+++
@@ -1,1 +0,0 @@
-SQLite database is created here automatically when the server starts. Keep this folder out of GitHub because it contains operational data.
--- theme.js
+++
@@ -1,9 +0,0 @@
-/* ---------- Theme ---------- */
-let currentTheme = localStorage.getItem('bqc_theme') || 'light';
-document.documentElement.setAttribute('data-theme', currentTheme);
-function setTheme(theme) {
-  currentTheme = theme;
-  localStorage.setItem('bqc_theme', theme);
-  document.documentElement.setAttribute('data-theme', theme);
-  render();
-}
